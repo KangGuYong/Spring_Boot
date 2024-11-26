845ab66528b2899b4ad478ce5e1cfadfd2b3786d
KangGuYong <rivenine123@naver.com>
2024-11-26 16:56:05 +0900

    3장 완료

--- mallapi/src/main/java/org/zerock/mallapi/repository/TodoRepository.java
+++ mallapi/src/main/java/org/zerock/mallapi/repository/TodoRepository.java
@@ -3,6 +3,6 @@ package org.zerock.mallapi.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.zerock.mallapi.domain.Todo;
 
-public interface TodoRepository
-        extends JpaRepository<Todo, Long> {
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+
 }
